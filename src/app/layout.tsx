61fcc61f033ade736cddae7ca69ef02543679079
Matt Casey <mattcasey8@gmail.com>
2025-07-25 21:08:33 +0000

    Can you check the favicon to make sure that it is the icon to the left o

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Playfair+Display:wght@700&display=swap" rel="stylesheet" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><path d=%22m8 6-4-4 4-4%22 fill=%22none%22 stroke=%22hsl(235 63% 30%)%22 stroke-width=%2210%22/><path d=%22m42 34 12 20 12-20-6-10-12 10z%22 fill=%22hsl(235 63% 30%)%22/></svg>" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 24 24%22 fill=%22none%22 stroke=%22hsl(235 63% 30%)%22 stroke-width=%222%22 stroke-linecap=%22round%22 stroke-linejoin=%22round%22><path d=%22m8 3 4 8 5-5 5 15H2L8 3z%22/></svg>" />
       </head>
       <body className="font-body bg-background text-foreground antialiased flex flex-col min-h-screen">
         <Header />
